98c428abe3124606093a09dc29ad4749b0d1be17
Adel Kawas <kawasadel@gmail.com>
2021-10-06 15:52:37 +0300

    Number Counter

---
+++ app.js
@@ -0,0 +1,21 @@
+document.querySelector("#numberChanger")
+let changeOfNumber = document.querySelector("#numberChanger");
+
+
+document.querySelector("#decrease");
+let lowerNumber = document.querySelector("#decrease");
+
+document.querySelector("#reset");
+let backToZero = document.querySelector("#reset");
+
+document.querySelector("#increase");
+let increaseNumber = document.querySelector("#increase");
+
+function numberChanger(){
+    increaseNumber = +i;
+
+
+
+
+}
+
