decf0fd1a626f0e47765d731d86d3a265a7751fe
Adel Kawas <kawasadel@gmail.com>
2021-10-06 16:55:22 +0300

    With For loop

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 document.querySelector("#numberChanger")
 let changeOfNumber = document.querySelector("#numberChanger");
-
+changeOfNumber.innerHTML = 1;
+console.log(changeOfNumber);
 
 document.querySelector("#decrease");
 let lowerNumber = document.querySelector("#decrease");
@@ -10,12 +11,19 @@ let backToZero = document.querySelector("#reset");
 
 document.querySelector("#increase");
 let increaseNumber = document.querySelector("#increase");
+increaseNumber.style.backgroundColor = "red";
 
-function numberChanger(){
-    increaseNumber = +i;
+// function incremental(){
+//     let i = 0;
+//     do {
+//       i += 1;
+//     } 
+// };
 
 
 
-
-}
-
+increaseNumber.addEventListener("click", function incremental(){
+    let changeOfNumber = i;
+    let i = 0
+    for (i = 0; i < 0;i++)
+});
